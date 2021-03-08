c612e3846ea6ea20d63c2bb88c23b72de9c24b53
lamhai1401 <lamthanhhai141@gmail.com>
2021-03-08 17:16:05 +0700

    init gin

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/lamhai1401/web-server.git
 
 go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-redis/redis/v8 v8.7.1
+)
--- main.go
+++ main.go
@@ -1,1 +1,69 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+)
+
+var red *redis.Client
+
+func main() {
+	app := gin.Default()
+
+	app.Use(useRateLimit(3, 10))
+
+	app.GET("/", func(c *gin.Context) {
+		c.JSON(200, "Hello world")
+	})
+
+	red = redis.NewClient(&redis.Options{
+		Addr:        "localhost:6379",
+		Password:    "",
+		DB:          0,
+		PoolTimeout: time.Minute, // since we user transaction so it can take a long time
+	})
+
+	app.Run(":8088")
+}
+
+func useRateLimit(rateLimit int64, second int64) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientIP := c.ClientIP()
+		key := "RATE_LIMIT_COUNT_" + clientIP
+		err := incRequestCount(key, rateLimit, second)
+
+		if err != nil {
+			c.AbortWithStatus(403)
+			return
+		}
+		c.Next()
+	}
+}
+
+func incRequestCount(key string, rateLimit, second int64) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := red.Watch(ctx, func(t *redis.Tx) error {
+		ctx2, cancel2 := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel2()
+		t.SetNX(ctx2, key, 0, time.Duration(second)*time.Second)
+		ctx3, cancel3 := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel3()
+		count, err := t.Incr(ctx3, key).Result()
+
+		if count > rateLimit {
+			err = fmt.Errorf("Rate limited")
+		}
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}, key)
+	return err
+}
